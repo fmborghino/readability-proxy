45b17912b7abb3e1d737f722036eede31df369ef
Max Borghino <fmborghino@gmail.com>
2013-11-11 21:53:54 -0800

    errors in html or json, more sinatra idioms

--- readability-proxy.rb
+++ readability-proxy.rb
@@ -13,15 +13,15 @@ before do
 end
 
 get '/' do
-  headers "Content-Type" => "text/html; charset=utf-8"
+  content_type 'text/html; charset=utf-8'
   haml :index
 end
 
-post '/v1/parser' do
+post '/v1/parser.?:format?' do
   parser
 end
 
-get '/v1/parser' do
+get '/v1/parser.?:format?' do
   parser
 end
 
@@ -29,31 +29,42 @@ private
 
 def parser
   token = ENV['READABILITY_PARSER_TOKEN'] || $config[:readability][:token]
-  url = "https://readability.com/api/content/v1/parser?url=%s&token=%s" % [ params[:url], token ]
-  uri = URI.parse(url)
-  http = Net::HTTP.new(uri.host, uri.port)
-  http.use_ssl = true
-  request = Net::HTTP::Get.new(uri.request_uri)
-  response = http.request(request)
-  if $config[:debug]
+  response = http_get "https://readability.com/api/content/v1/parser?url=%s&token=%s" % [ params[:url], token ]
+  if debug?
     puts "RESPONSE CODE %s"%response.code
     puts "HEADERS"
     response.each_header {|key,value| puts "#{key}: #{value}" }
     puts "BODY"
     puts response.body
   end
-  if response.code != "200"
-    return "OOPS: code %s</br>%s" % [response.code, response.body]
+  body = JSON.parse(response.body) rescue { error: true, message: "body not json: %s" % response.body[0..50] }
+  if response.code != "200" or body.has_key? :error
+    if params[:format] == 'html'
+      content_type "text/html; charset=utf-8"
+      return haml :parser_error, locals: { response: response, body: body }
+    else
+      content_type "application/json"
+      return [ response.code.to_i, [ body.to_json ] ]
+    end
   end
-  body = JSON.parse(response.body)
-  if body["error"]
-    return "OOPS: %s" % body["messages"]
-  end
+
   if params[:format] == 'html'
-    headers "Content-Type" => "text/html; charset=utf-8"
-    "<p>(<b>%s</b>) %s</p><hr/>" % [response.code, body["title"]] + body["content"]
+    content_type "text/html; charset=utf-8"
+    "<p><b>%s</b></p><hr/>" % body["title"] + body["content"]
   else
-    headers "Content-Type" => "application/json"
+    content_type "application/json"
     response.body
   end
+end
+
+def http_get url
+  uri = URI.parse(url)
+  http = Net::HTTP.new(uri.host, uri.port)
+  http.use_ssl = true
+  request = Net::HTTP::Get.new(uri.request_uri)
+  http.request(request)
+end
+
+def debug?
+  (not ENV['DEBUG'].nil?) || $config[:debug]
 end
