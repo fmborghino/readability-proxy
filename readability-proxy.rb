caa6658da08245c8633cd4c41e30203a00fb43da
Max Borghino <fmborghino@gmail.com>
2013-12-04 23:34:17 -0800

    Move successful content to haml.
     - several cleanups to haml invocation
     - add original link to error page

--- readability-proxy.rb
+++ readability-proxy.rb
@@ -7,9 +7,7 @@ require 'sinatra'
 $config = YAML.load_file(File.join(Dir.pwd, 'config.yml'))
 
 STYLE = <<EOS
-<style type="text/css">
   img { max-width:100%; }
-</style>
 EOS
 
 configure do
@@ -35,44 +33,50 @@ private
 
 def parser
   token = ENV['READABILITY_PARSER_TOKEN'] || $config[:readability][:token]
-  response = http_get "https://readability.com/api/content/v1/parser?url=%s&token=%s" % [ params[:url], token ]
-  if debug?
-    puts "RESPONSE CODE %s"%response.code
-    puts "HEADERS"
-    response.each_header {|key,value| puts "#{key}: #{value}" }
-    puts "BODY"
-    puts response.body
-  end
-  body = JSON.parse(response.body) rescue { error: true, message: "body not json: %s" % response.body[0..50] }
-  if response.code != "200" or body.has_key? :error
-    body['code'] = response.code.to_i
+  @parser_resp = http_get "https://readability.com/api/content/v1/parser?url=%s&token=%s" % [ params[:url], token ]
+  dump_debug(@parser_resp) if debug?
+  @parser_resp_body = JSON.parse(@parser_resp.body) rescue { error: true, message: "body not json: %s" % @parser_resp.body[0..50] }
+
+  if @parser_resp.code != "200" or @parser_resp_body.has_key? :error
+    @original_url = params[:url]
+    @parser_resp_body['code'] = @parser_resp.code.to_i
+    @parser_resp_body['url'] = @original_url
     if params[:format] == 'html'
       content_type "text/html; charset=utf-8"
-      return haml :parser_error, locals: { response: response, body: body }
+      return haml :parser_error
     else
       content_type "application/json"
-      return [ response.code.to_i, [ body.to_json ] ]
+      return [ @parser_resp.code.to_i, [ @parser_resp_body.to_json ] ]
     end
   end
 
   if params[:format] == 'html'
     content_type "text/html; charset=utf-8"
-    title = body['title'] || '[Full version]'
-    STYLE + "<p><a href=\"%s\" target=\"_blank\"><b>%s</b></a></p><hr/>" % [body['url'], title] + body['content']
+    @title = @parser_resp_body['title'] || '[Full version]'
+    @style = STYLE
+    haml :content
   else
     content_type "application/json"
-    response.body
+    @parser_resp.body
   end
 end
 
 def http_get url
   uri = URI.parse(url)
   http = Net::HTTP.new(uri.host, uri.port)
-  http.use_ssl = true
+  http.use_ssl = (uri.scheme == 'https')
   request = Net::HTTP::Get.new(uri.request_uri)
   http.request(request)
 end
 
 def debug?
   (not ENV['DEBUG'].nil?) || $config[:debug]
+end
+
+def dump_debug r
+  puts "RESPONSE CODE %s" % r.code
+  puts "HEADERS"
+  r.each_header {|key,value| puts "#{key}: #{value}" }
+  puts "BODY"
+  puts r.body
 end
